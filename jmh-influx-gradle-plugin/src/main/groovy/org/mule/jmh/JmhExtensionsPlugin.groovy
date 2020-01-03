60e82658bf6719141e0529e70df0fa9e58f81aa7
Leandro Shokida <leandroshokida@gmail.com>
2020-01-03 19:49:20 -0300

    Changes for gradle 5.0

--- jmh-influx-gradle-plugin/src/main/groovy/org/mule/jmh/JmhExtensionsPlugin.groovy
+++ jmh-influx-gradle-plugin/src/main/groovy/org/mule/jmh/JmhExtensionsPlugin.groovy
@@ -8,11 +8,13 @@ import org.mule.jmh.report.influx.InfluxReporter
 class JmhExtensionsPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.extensions.create('influxReport', InfluxReportExtension)
-        def task = project.task('generateJmhInfluxReport') << {
-            InfluxReportExtension configuration = project.influxReport
-            configuration.dbName = configuration.dbName ?: project.name + "_db"
-            project.logger.info 'Using this configuration:\n{}', configuration
-            new InfluxReporter().createReport(project.buildDir.absolutePath + "/" + configuration.reportPath, configuration.dbName, new InfluxConnectionProperties(configuration.dbUrl, configuration.dbUserName, configuration.dbUserPassword))
+        def task = project.task('generateJmhInfluxReport') {
+            doLast {
+                InfluxReportExtension configuration = project.influxReport
+                configuration.dbName = configuration.dbName ?: project.name + "_db"
+                project.logger.info 'Using this configuration:\n{}', configuration
+                new InfluxReporter().createReport(project.buildDir.absolutePath + "/" + configuration.reportPath, configuration.dbName, new InfluxConnectionProperties(configuration.dbUrl, configuration.dbUserName, configuration.dbUserPassword))
+            }
         }
         task.group = "jmh"
         task.description = "Parses the json result and inserts it inside an influx db."
